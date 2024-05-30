c692e89ae385e1aefff49f9b049e4d1956fcfe3b
Jakub Pająk <jakub.pajak@microsoft.wsei.edu.pl>
2024-05-30 19:32:49 +0200

    Added new algorithms

---
+++ Algorithms/Bisection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorytmy.Algorithms
+{
+    internal class Bisection
+    {
+        public static double BisectionMethod(Func<double, double> func, double left, double right, double tolerance, double a, double b, double c, double d, double e, double f)
+        {
+            Func<double, double> polynomial = (x) =>
+            a * Math.Pow(x, 5) +
+            b * Math.Pow(x, 4) +
+            c * Math.Pow(x, 3) +
+            d * Math.Pow(x, 2) +
+            e * x +
+            f;
+
+            // Sprawdzenie, czy funkcja zmienia znak na końcach przedziału
+            if (func(left) * func(right) >= 0)
+            {
+                throw new ArgumentException("Funkcja musi zmieniać znak na końcach przedziału [left, right]");
+            }
+
+            double mid = left;
+
+            // Liczenie pierwiastka
+            while ((right - left) / 2.0 > tolerance)
+            {
+                mid = (left + right) / 2.0;
+                if (func(mid) == 0.0)
+                {
+                    break; // Znaleziono dokładny pierwiastek
+                }
+                else if (func(left) * func(mid) < 0)
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid;
+                }
+            }
+
+            return mid;
+        }
+    }
+}
---
+++ Algorithms/MonteCarlo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorytmy.Algorithms
+{
+    internal class MonteCarlo
+    {
+        public static double MonteCarloMethod(int points)
+        {
+            // Definiowanie funkcji, dla której chcemy obliczyć pole pod wykresem
+            Func<double, double> function = x =>
+            Math.Sin(x); // Przykład: funkcja sinus
+
+            // Zakresy, w których chcemy obliczyć pole pod wykresem
+            double xMin = 0;
+            double xMax = Math.PI;
+            double yMin = 0;
+            double yMax = 1;
+
+            // Liczba punktów do wygenerowania w algorytmie Monte Carlo
+            points = 1000000;
+
+            // Licznik punktów znajdujących się pod wykresem funkcji
+            int pointsUnderCurve = 0;
+
+            // Obiekt do generowania liczb losowych
+            Random random = new Random();
+
+            // Generowanie losowych punktów i sprawdzanie, czy znajdują się pod wykresem funkcji
+            for (int i = 0; i<points; i++)
+            {
+                double x = xMin + (xMax - xMin) * random.NextDouble();
+                double y = yMin + (yMax - yMin) * random.NextDouble();
+
+                if (y <= function(x))
+                {
+                    pointsUnderCurve++;
+                }
+            }
+
+            // Obliczenie pola prostokąta obejmującego wykres funkcji
+            double rectangleArea = (xMax - xMin) * (yMax - yMin);
+
+            // Obliczenie pola pod wykresem funkcji
+            double areaUnderCurve = (double)pointsUnderCurve / points * rectangleArea;
+
+            Console.WriteLine($"Pole pod wykresem funkcji: {areaUnderCurve}");
+
+            return 0;
+        }
+    }
+}
